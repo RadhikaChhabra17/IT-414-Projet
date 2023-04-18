a1537ab3d18b08660e04400ed10250181d7b9146
RadhikaChhabra17 <77332750+RadhikaChhabra17@users.noreply.github.com>
2023-04-18 23:53:19 +0530

    Update histogram.py

--- algorithms/clustering/quantile_histogram/histogram.py
+++ algorithms/clustering/quantile_histogram/histogram.py
@@ -4,65 +4,9 @@ import scipy.stats as ss
 
 
 class QuantileHistogram(object):
-    """
-    A class used to represent an equi depth quantile histogram
 
-     e.g. Quantile histogram with 256 buckets
-
-    |------||------||------| ... |------|
-        1       2       3    ...    256
-
-
-    Attributes
-    ----------
-
-
-
-    Methods
-    -------
-    create_histogram(column: list, quantiles: int)
-        Creates an equi depth quantile histogram for the specified column with the specified amount of quantiles
-
-    create_histogram_from_reference(column: list)
-        Creates an equi depth quantile histogram for the specified column based on the reference histogram
-
-    get_buckets()
-        Returns the histogram's buckets
-
-    chunks(lst: ndarray, n: int)
-        Splits a list into n equal sized chunks
-
-    get_sorted_ranks(column: list)
-         A UNIX-like "sort -n" function that works for mixed columns that might contain both numbers and strings.
-        The strings are sorted first in lexicographic order and then the numbers in numeric and then it returns the
-        ranks of the sorted data
-
-    copy_buckets_without_values()
-        Copies the bucket boundaries but not the values
-
-     add_value_to_bucket(value: float)
-        Adds a value into a bucket
-
-     normalize_buckets(column_size: int)
-        Normalizes the frequencies inside the buckets based on the column size
-
-     print_histogram()
-        Prints the histogram
-
-     flatten_histogram()
-        Flattens the histogram by creating two lists with all the bucket values with their weights
-    """
     def __init__(self, name: str, ranks: ndarray, normalization: int, quantiles: int, reference_hist=None):
-        """
-        Parameters
-        ----------
-        ranks : list
-            the column's ranked data
-        quantiles : int
-            the number of quantiles
-        reference_hist : QuantileHistogram, optional
-            the reference histogram that provides the bucket boundaries
-        """
+
         self.bucket_boundaries = {}
         self.bucket_values = {}
         self.name = name
